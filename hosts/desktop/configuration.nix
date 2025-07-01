6a19b25af98b7f171dae8f3097f935131e4d6bf9
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2025-06-30 22:15:58 -0400

    change kde for niri

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,7 +10,7 @@
 	networking.hostName = "desktop";
 
 	services.displayManager.sddm.enable = true;
-	services.displayManager.ssdm.wayland.enable = true;
+	services.displayManager.sddm.wayland.enable = true;
 	/*
 	# Enable the KDE Plasma Desktop Environment.
 	services.desktopManager.plasma6.enable = true;
