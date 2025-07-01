e8545b2616c90baf973c78aa019ba59b54216b53
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2025-06-30 23:40:59 -0400

    go back to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "the very best config for nixos";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
 	};
 	outputs = { self, nixpkgs }@inputs : {
