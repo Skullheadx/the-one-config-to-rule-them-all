2d20bc32417e5ba7727bf3aadb60da64ddf8cb4f
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2025-07-01 00:10:31 -0400

    remove option that doesn't exist

--- nixosModules/system/sddm.nix
+++ nixosModules/system/sddm.nix
@@ -2,6 +2,4 @@
 	# Simple Desktop Display Manager
 	services.displayManager.sddm.enable = true;
 	services.displayManager.sddm.wayland.enable = true;
-	services.displayManager.sddm.autoNumLock = true;
-
 }
