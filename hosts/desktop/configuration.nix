039677930190adf30f617b28b1b6831dbed4d504
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2025-06-30 22:44:52 -0400

    add hyprland

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,7 +10,7 @@
 	networking.hostName = "desktop";
 
 	services.displayManager.sddm.enable = true;
-	services.displayManager.sddm.wayland.enable = true;
+	services.displayManager.sddm.xwayland.enable = true;
 	# Enable the KDE Plasma Desktop Environment.
 	services.desktopManager.plasma6.enable = true;
 
@@ -49,6 +49,7 @@
 	];
 
 	programs.niri.enable = true;
+	programs.hyprland.enable = true;
 
 
 	# Some programs need SUID wrappers, can be configured further or are
